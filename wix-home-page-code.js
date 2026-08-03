576c0a962f99ff8d6c842138aae0c2ab53c85c31
HydroPip <info@hydropip.com>
2026-08-03 01:10:10 -0400

    Refine hero system photo caption

--- wix-home-page-code.js
+++ wix-home-page-code.js
@@ -2,7 +2,7 @@ import wixWindowFrontend from "wix-window-frontend";
 import wixLocation from "wix-location";
 import { currentMember, authentication } from "wix-members-frontend";
 
-const HYDROPIP_HOME_SRC = "https://hydropip-pip-api.onrender.com/home.html?v=launch-20260803b";
+const HYDROPIP_HOME_SRC = "https://hydropip-pip-api.onrender.com/home.html?v=launch-20260803c";
 const HOME_EMBED_IDS = ["#homeHtml", "#html1", "#html2", "#iFrame1"];
 
 $w.onReady(() => {
